294982324f16b3d99da84e6f1d2c36ec77c1e1e7
Stephane Nicoll <snicoll@vmware.com>
2021-10-02 08:15:28 +0200

    Upgrade Java 8 version in CI image
    Closes gh-66

--- ci/images/setup.sh
+++ ci/images/setup.sh
@@ -19,7 +19,7 @@ curl https://raw.githubusercontent.com/spring-io/concourse-java-scripts/v0.0.2/c
 ###########################################################
 # JAVA
 ###########################################################
-JDK_URL="https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz"
+JDK_URL="https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz"
 
 
 mkdir -p /opt/openjdk
